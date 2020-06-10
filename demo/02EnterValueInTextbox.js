34ac150c117af151dc7a86f0aa5e3bab0c55be89
Jon Woo <JWoo@eleadcrm.com>
2020-06-10 17:48:59 -0500

    fix clicking

--- demo/02EnterValueInTextbox.js
+++ demo/02EnterValueInTextbox.js
@@ -1,11 +1,11 @@
 module.exports = {
-    'Can I enter a value into a textbox' : browser => {
-      browser
-        .url('https://www.phptravels.net/register')
-        // we need to wait for the web page to load in the browser before we test anything
-        .waitForElementVisible('body')
-        .setValue('input[name=firstname]', 'bob')
-        // i am waiting for a fake element so that the browser will  not close for demo purposes
-        .waitForElementVisible('sdfsdfdsf',99999);
-    }
-  };
+  'Can I enter a value into a textbox' : browser => {
+    browser
+      .url('https://www.phptravels.net/register')
+      // we need to wait for the web page to load in the browser before we test anything
+      .waitForElementVisible('body')
+      .setValue('input[name=firstname]', 'bob')
+      // i am waiting for a fake element so that the browser will  not close for demo purposes
+      .waitForElementVisible('sdfsdfdsf',99999);
+  }
+};
--- demo/03ClickAButton.js
+++ demo/03ClickAButton.js
@@ -1,9 +1,12 @@
 module.exports = {
     'Can I click a button' : browser => {
       browser
-      .url('https://www.phptravels.net/register')
-      .waitForElementVisible('button[type=submit]')
-      .click('button[type=submit]')
+      .url('http://the-internet.herokuapp.com/add_remove_elements/')
+      .waitForElementVisible('body')
+      // click the button 3 times to add 3 other elements
+      .click('.example button')
+      .click('.example button')
+      .click('.example button')
       // i am waiting for a fake element so that the browser will  not close for demo purposes
       .waitForElementVisible('sdfsdfdsf',99999);
     }
